030ac4d3202d017230dd602a5631951ddb9a8f73
vladimir <vladimir@safing.io>
2023-02-09 18:11:26 +0200

    Add KeepAlive option documentation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -12,7 +12,7 @@ const config: CapacitorConfig = {
   // },
   cordova: {
     preferences: {
-      "KeepAlive": "false"
+      "KeepAlive": "false" // Determines whether the application stays running in the background even after a pause event fires. Setting this to false does not kill the app after a pause event, but simply halts execution of code within the cordova webview while the app is in the background. (https://cordova.apache.org/docs/en/latest/config_ref/)
     }
   }
 };
